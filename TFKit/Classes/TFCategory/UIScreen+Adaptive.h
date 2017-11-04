b3b3e887509b5a9aa52d5a6bb22d8196d67e4598
fengtengfei <1005052145@qq.com>
2017-11-04 10:02:27 +0800

    add

---
+++ TFKit/Classes/TFCategory/UIScreen+Adaptive.h
@@ -0,0 +1,20 @@
+//
+//  UIScreen+Adaptive.h
+//  AFDownloadRequestOperation-iOS8.0
+//
+//  Created by Fengtf on 2017/11/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScreen (Adaptive)
+
++ (CGFloat)homeIndeicatrHeight;
+
++ (CGFloat)width;
+
++ (BOOL)iPhoneX;
+
++ (CGFloat)statusBarHeight;
+
+@end
---
+++ TFKit/Classes/TFCategory/UIScreen+Adaptive.m
@@ -0,0 +1,31 @@
+//
+//  UIScreen+Adaptive.m
+//  AFDownloadRequestOperation-iOS8.0
+//
+//  Created by Fengtf on 2017/11/2.
+//
+
+#import "UIScreen+Adaptive.h"
+
+@implementation UIScreen (Adaptive)
+
++ (CGFloat)width {
+    return [self mainScreen].bounds.size.width;
+}
+
++ (BOOL)iPhoneX {
+    if (@available(iOS 11.0, *)){
+        return [UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone && (NSInteger)[UIScreen mainScreen].nativeBounds.size.height == 2436; //nativeBounds
+    }
+    return NO;
+}
+
++ (CGFloat)homeIndeicatrHeight {
+    return [self iPhoneX] ? 34 : 0;
+}
+
++ (CGFloat)statusBarHeight {
+    return [self iPhoneX] ? 44 : 20;
+}
+
+@end
